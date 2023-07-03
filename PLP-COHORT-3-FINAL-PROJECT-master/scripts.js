7bd765f2c5d4f4c669d5d69c83f12fd14634db1e
Patrick Wafula Munuku <arnoldkells4@gmail.com>
2023-07-03 03:36:28 -0700

    Work Still underway

--- PLP-COHORT-3-FINAL-PROJECT-master/scripts.js
+++ PLP-COHORT-3-FINAL-PROJECT-master/scripts.js
@@ -1,14 +1,13 @@
-
-//  var swiper = new Swiper(".service-slide", {
-//     spaceBetween: 30,
-//     centeredSlides: true,
-//     autoplay: {
-//       delay: 7500,
-//       disableOnInteraction: false,
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     loop:true,
-//   });
+var swiper = new Swiper(".home-slide", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
